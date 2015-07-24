e3ebe261eef1560cc85c51670a3cb9ceeb06f726
Andrew Proudian <aproudia@mines.edu>
2015-07-24 17:02:43 -0600

    Added "cform" checking. Improved formatting of "rng" object for use in plotting. Reduced default resolution to 400, which appears more in line with spectral matching with envelopes; more testing to be done here.

--- R/rapt-spec.R
+++ R/rapt-spec.R
@@ -1,5 +1,9 @@
 # Define ranges in MassSpectrum
-rangeSpec <- function(form, charge = 1:4, resolution = 600) {
+rangeSpec <- function(form, charge = 1:4, resolution = 400) {
+  if(is(form, "cform")) {
+    cform <- as.data.frame(t(form));
+    form <- form["formula", ];
+  }
   rng.pat <- lapply(charge, function(x){
     enviPat::isopattern(
       isotopes, chemforms = form,
@@ -12,18 +16,41 @@ rangeSpec <- function(form, charge = 1:4, resolution = 600) {
     frac = 0.1, env = "CauchyLorentz", resolution = resolution,
     verbose = F
   )
-  rng.dat <- lapply(rng.env, function(x){
+  rng.raw <- lapply(rng.env, function(x){
     lapply(x, function(y){
       range(subset(y, select = "m/z", subset = "abundance" > 20))
     })
   })
-  return(rng.dat)
+  rng.dat <- lapply(charge, function(x){
+    lapply(seq_along(rng.raw[[x]]), function(y){
+      data.frame(
+        formula = names(rng.raw[[x]])[y],
+        charge = x,
+        start = rng.raw[[x]][[y]][1],
+        end = rng.raw[[x]][[y]][2]
+      );
+    })
+  })
+  rng.dat <- do.call(rbind, unlist(rng.dat, recursive = F));
+  rng.dat <- rng.dat[order(rng.dat$formula,rng.dat$charge),];
+  rownames(rng.dat) <- apply(rng.dat, 1, function(x){
+    paste(c(x["formula"], x["charge"]), collapse = "+");
+  })
+  if (exists("cform")) {
+    rng.dat$color <- as.character(cform$color[match(rng.dat$formula, cform$formula)]);
+  } else {
+    rng.dat$color <- rep_len(NA, dim(rng.dat)[1]);
+  }
+  return(rng.dat);
 }
 # Create a reference MassPeaks object for warping
-refSpec <- function(form, charge = 1:4, resolution = 600) {
+refSpec <- function(form, charge = 1:4, resolution = 400) {
+  if(is(form,"cform")) {
+    form <- form["formula", ];
+  }
   ref.pat <- lapply(charge, function(x){
     enviPat::isopattern(
-      isotopes, chemforms = form,
+      isotopes, chemforms = form["formula", ],
       threshold = 0.01, charge = x,
       verbose = F
     )
@@ -39,6 +66,6 @@ refSpec <- function(form, charge = 1:4, resolution = 600) {
     plotit = F, verbose = F
   )
   ref.vdet <- do.call(rbind,unlist(ref.vdet, recursive = F))
-  ref.dat <- createMassPeaks(ref.vdet[,"m/z"],ref.vdet[,"abundance"])
+  ref.dat <- createMassPeaks(ref.vdet[, "m/z"], ref.vdet[, "abundance"])
   return(ref.dat)
 }
