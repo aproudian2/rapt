0f8009918ee679f3937d8dd7e0673cc1b8f6b8b9
Andrew Proudian <aproudia@mines.edu>
2015-07-23 22:52:18 -0600

    Added metaData stub for POS import and xfer to MassSpec

--- R/rapt-file.R
+++ R/rapt-file.R
@@ -3,11 +3,17 @@
 readPOS <- function(filepath) {
   pos.len <- file.info(filepath)['size'] / 4;
   pos.len <- as.numeric(pos.len);
-  pos.raw <- readBin(filepath, what = "numeric",
-                     size = 4, endian = "big", n = pos.len);
+  pos.raw <- readBin(
+    filepath, what = "numeric",
+    size = 4, endian = "big", n = pos.len
+  );
   pos.mat <- matrix(pos.raw, ncol = 4, byrow = T);
   pos.dat <- as.data.frame(pos.mat);
   names(pos.dat) <- c("x", "y", "z", "mass");
+  pos.name <- sub(".pos", "", basename(filepath));
+  attr(pos.dat, "meta") <- list(
+    name = pos.name
+  )
   return(pos.dat);
 }
 # Create a list of mass spec formulas from a table
@@ -34,7 +40,10 @@ createSpec <- function(pos, res = 0.001) {
   ms.range <- range(pos["mass"]);
   ms.breaks <- seq(ms.range[1], ms.range[2], res)
   ms.hist <- hist(pos, ms.breaks, plot = F);
-  ms.dat <- createMassSpectrum(ms.hist$mids, ms.hist$counts); #Meta-data
+  ms.dat <- createMassSpectrum(
+    ms.hist$mids, ms.hist$counts,
+    metaData = attr(pos,"meta")
+  );
   return(ms.dat);
 }
 ### Write Data ###
