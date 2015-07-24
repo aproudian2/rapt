5f50b106b790984f299bd010078d5e18c66fedba
Andrew Proudian <aproudia@mines.edu>
2015-07-23 23:42:13 -0600

    Added reference peak function for warping mass spectra. Changed charge states to be user defined, with a default of 1:4.

--- R/rapt-spec.R
+++ R/rapt-spec.R
@@ -1,6 +1,6 @@
 # Define ranges in MassSpectrum
-rangeSpec <- function(form, resolution = 600) {
-  rng.pat <- lapply(1:4, function(x){
+rangeSpec <- function(form, charge = 1:4, resolution = 600) {
+  rng.pat <- lapply(charge, function(x){
     enviPat::isopattern(
       isotopes, chemforms = form,
       threshold = 0.01, charge = x,
@@ -18,4 +18,27 @@ rangeSpec <- function(form, resolution = 600) {
     })
   })
   return(rng.dat)
+}
+# Create a reference MassPeaks object for warping
+refSpec <- function(form, charge = 1:4, resolution = 600) {
+  ref.pat <- lapply(charge, function(x){
+    enviPat::isopattern(
+      isotopes, chemforms = form,
+      threshold = 0.01, charge = x,
+      verbose = F
+    )
+  })
+  ref.env <- lapply(
+    ref.pat, enviPat::envelope,
+    frac = 0.1, env = "CauchyLorentz", resolution = resolution,
+    verbose = F
+  )
+  ref.vdet <- lapply(
+    ref.env, enviPat::vdetect,
+    detect = "intensoid",
+    plotit = F, verbose = F
+  )
+  ref.vdet <- do.call(rbind,unlist(ref.vdet, recursive = F))
+  ref.dat <- createMassPeaks(ref.vdet[,"m/z"],ref.vdet[,"abundance"])
+  return(ref.dat)
 }
