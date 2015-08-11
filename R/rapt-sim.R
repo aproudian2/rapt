25d7ef9be58f0e91617da9ae223af44cda140ea5
Andrew Proudian <aproudia@mines.edu>
2015-08-11 13:50:04 -0600

    Implemented the shift and rPoissonCluster methods for class pp3

--- R/rapt-sim.R
+++ R/rapt-sim.R
@@ -3,12 +3,42 @@
 #
 
 # Extends the superimpose function from "SpatStat" to handle pp3
-superimpose.pp3 <- function(..., W = NULL) {
+superimpose.pp3 <- function(..., W = NULL, check = F) {
   input.list <- list(...)
   df.list <- lapply(input.list, as.data.frame)
   df.comb <- Reduce(rbind, df.list)
   out.pp3 <-  createSpat(df.comb, win = W)
   return(out.pp3)
+}
+# Extends the shift function from "SpatStat" to handle pp3
+shift.pp3 <- function (X, vec = c(0, 0, 0), ..., origin = NULL)
+{
+  verifyclass(X, "pp3")
+  if (!is.null(origin)) {
+    if (!missing(vec))
+      warning("argument vec ignored; overruled by argument origin")
+    if (is.numeric(origin)) {
+      locn <- origin
+    }
+    else if (is.character(origin)) {
+      origin <- pickoption("origin", origin,
+                           c(midpoint = "midpoint", bottomleft = "bottomleft"))
+      W <- X$domain
+      locn <- switch(origin, midpoint = {
+        c(mean(W$domain$xrange), mean(W$domain$yrange), mean(W$domain$zrange))
+      }, bottomleft = {
+        c(W$domain$xrange[1], W$domain$yrange[1], W$domain$zrange[1])
+      })
+    }
+    else stop("origin must be a character string or a numeric vector")
+    vec <- -locn
+  }
+  Y <- pp3(X$data$x + vec[1], X$data$y + vec[2], X$data$z + vec[3],
+           xrange = X$domain$xrange + vec[1],
+           yrange = X$domain$yrange + vec[2],
+           zrange = X$domain$zrange + vec[3])
+  attr(Y, "lastshift") <- vec
+  return(Y)
 }
 # Extends the rpoint function from "SpatStat" to handle pp3
 rpoint3 <- function (n, f, fmax = 1,  win = box3(), ...,
@@ -69,10 +99,72 @@ rpoint3 <- function (n, f, fmax = 1,  win = box3(), ...,
   names(result) <- paste("Simulation", 1:nsim)
   return(as.anylist(result))
 }
-# Generalization of the "SpatStat" cluster generators to pp3
-# Modeled off of rPoissonCluster
-rCluster <- function(kappa, expand, rcluster, win = box3(), ...,
-                     lmax = NULL, nsim = 1, drop = T)
+# Extends the rPoissonCluster function from "SpatStat" to pp3
+rPoissonCluster3 <- function(kappa, expand, rcluster, win = box3(), ...,
+                             nsim = 1, drop = T)
 {
-
+  if (missing(expand) && !is.null(rmax <- list(...)$rmax)) {
+    expand <- rmax
+    f <- rcluster
+    rcluster <- function(..., rmax) f(...)
+  }
+  verifyclass(win, "box3")
+  frame <- boundingbox(win)
+  dilated <- box3(win$xrange + c(-expand, expand),
+                  win$yrange + c(-expand, expand),
+                  win$yrange + c(-expand, expand))
+  parentlist <- rpoispp3(kappa, domain = dilated,
+                        nsim = nsim)
+  if (nsim == 1)
+    parentlist <- list(parentlist)
+  resultlist <- vector(mode = "list", length = nsim)
+  for (isim in 1:nsim) {
+    parents <- parentlist[[isim]]
+    result <- NULL
+    res.full <- NULL
+    np <- dim(parents$data)[1]
+    if (np > 0) {
+      xparent <- parents$data$x
+      yparent <- parents$data$y
+      zparent <- parents$data$z
+      for (i in seq_len(np)) {
+        cluster <- rcluster(...)
+        if (dim(cluster$data)[1] > 0) {
+          cluster <- shift(cluster, vec = c(xparent[i], yparent[i], zparent[i]))
+          cluster <- pp3(cluster$data$x, cluster$data$y, cluster$data$z,
+                         xrange = win$xrange,
+                         yrange = win$yrange,
+                         zrange = win$zrange)
+          clus.trunc <- subset(cluster, subset =
+                                 (x > win$xrange[1] & x < win$xrange[2]) &
+                                 (y > win$yrange[1] & y < win$yrange[2]) &
+                                 (z > win$zrange[1] & z < win$zrange[2])
+                               )
+          if (is.null(result)) {
+            result <- clus.trunc
+            res.full <- cluster
+            parentid <- rep.int(1, dim(clus.trunc$data)[1])
+          }
+          else {
+            result <- superimpose(result, clus.trunc, W = win)
+            res.full <- superimpose(res.full, cluster, W = win)
+            parentid <- c(parentid, rep.int(i, dim(clus.trunc$data)[1]))
+          }
+        }
+      }
+    }
+    else {
+      result <- pp3(numeric(0), numeric(0), numeric(0), window = win)
+      parentid <- integer(0)
+    }
+    attr(result, "parents") <- parents
+    attr(result, "parentid") <- parentid
+    attr(result, "expand") <- expand
+    attr(result, "full") <- res.full
+    resultlist[[isim]] <- result
+  }
+  if (nsim == 1 && drop)
+    return(resultlist[[1]])
+  names(resultlist) <- paste("Simulation", 1:nsim)
+  return(as.anylist(resultlist))
 }
