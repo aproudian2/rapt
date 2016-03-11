22067bb1d4d90bbcb5486f4f3c52e16d661c5a12
Andrew Proudian <aproudia@mines.edu>
2016-03-10 17:24:37 -0700

    Added readATO() which reads a gnereated ATO file from IVAS; amended createSpat() to accomodate the ATO format as well

--- R/rapt-file.R
+++ R/rapt-file.R
@@ -20,6 +20,26 @@ readPOS <- function(filepath) {
   )
   return(pos.dat);
 }
+# Read an ATO file (from IVAS reconstruction) into a data frame
+readATO <- function(filepath) {
+  ato.len <- file.info(filepath)['size'] / 4;
+  ato.len <- as.numeric(ato.len);
+  ato.file <- file(filepath, open = "rb");
+  seek(ato.file, where = 8);
+  ato.raw <- readBin(
+    ato.file, what = "numeric",
+    size = 4, endian = "little", n = ato.len
+  );
+  ato.mat <- matrix(ato.raw, ncol = 14, byrow = T);
+  ato.dat <- as.data.frame(ato.mat);
+  names(ato.dat) <- c("x", "y", "z", "mass", "clusID", "pIndex", "Vdc", "TOF", "dx", "dy", "Vp", "shank", "FouR", "FouI");
+  ato.name <- sub(".ato", "", basename(filepath));
+  attr(ato.dat, "metaData") <- list(
+    name = ato.name
+  )
+  close(ato.file);
+  return(ato.dat);
+}
 # Create a list of mass spec formulas from a table (class "cform")
 readForm <- function(filepath) {
   data("isotopes");
@@ -38,7 +58,7 @@ readForm <- function(filepath) {
 createSpat <- function(pos, win = NULL) {
   pp3.box <- win;
   if(is.null(win)) {
-    pp3.box <- sapply(pos[-4], range);
+    pp3.box <- sapply(pos[1:3], range);
   }
   pp3.dat <- pp3(pos$x, pos$y, pos$z, pp3.box);
   attr(pp3.dat, "metaData") <- attr(pos, "metaData");
