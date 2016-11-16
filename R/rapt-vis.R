f37c277928476ed8770123837092b6f284341aae
Andrew Proudian <aproudia@mines.edu>
2016-11-16 14:40:07 -0700

    Line break clean-up and comments

--- R/rapt-vis.R
+++ R/rapt-vis.R
@@ -6,6 +6,7 @@
 # Shade regions under a MassSpectrum
 # Based on Gavin Simpson:
 # www.fromthebottomoftheheap.net/2013/01/11/shading-regions-under-a-curve
+# Add ability to shade regions
 polyCurve <- function(x, y, from, to, n = 50, miny,
                       col = "red", border = NA) {
   drawPoly <- function(fun, from, to, n = 50, miny, col, border) {
@@ -28,16 +29,24 @@ polyCurve <- function(x, y, from, to, n = 50, miny,
   invisible();
 }
 # Plot a pretty MassSpectrum
-prettyPlot <- function(ms, rng = NA,  xlim = NULL, ylim = NULL, add = F) {
+# This assumes a log10 transformed spectrum
+prettyPlot <- function(ms, rng = NA,  xlim = NULL, ylim = NULL, add = F,
+                       main = "Mass Spectrum") {
+  if(log10(max(ms@intensity)) > 1) {
+    stop("prettyPlot should only be used with log10 transformed spectra.")
+  }
   ms.old <- par(no.readonly = T);
   par(mgp = c(2.2, 0.5, 0), cex = 1.5, las =  1, mar = c(3.5, 3.5, 1.5, 1));
   plot(ms, yaxt = "n", tck = -0.02, xlim = xlim, ylim = ylim,
        xlab = "Mass-to-Charge-State Ratio (Da)", ylab = "Counts (arb.)",
-       main = "Mass Spectrum");
+       main = main);
   ms.lims <- par("usr");
-  ms.exp <- sapply(1:ms.lims[4], function(x){paste0(c("10^", x), collapse = "")});
+  ms.exp <- sapply(1:ms.lims[4], function(x){
+    paste0(c("10^", x), collapse = "")
+  });
   axis(side = 2, at = 1:ms.lims[4], labels = NA, tck = -0.02);
-  axis(side = 2, at = 1:ms.lims[4], labels = parse(text = ms.exp), las = 1, lwd = 0);
+  axis(side = 2, at = 1:ms.lims[4], labels = parse(text = ms.exp),
+       las = 1, lwd = 0);
   ms.tck <- lapply(1:2, axTicks);
   ms.diff <- ms.tck[[1]][2] - ms.tck[[1]][1];
   ms.tck[[1]] <- seq(
@@ -56,6 +65,9 @@ prettyPlot <- function(ms, rng = NA,  xlim = NULL, ylim = NULL, add = F) {
   par(ms.old);
 }
 
+### Spatial Points ###
+# Plot a pp3 in a manipulatable 3D plot
+# (requires the rgl library)
 plot3d.pp3 <- function(X, ...) {
   rgl::plot3d(as.data.frame(X$data), ...)
 }
