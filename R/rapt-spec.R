b16fe889153e7ae521a894f7b09f57d820a9624f
Andrew Proudian <aproudia@mines.edu>
2017-12-01 14:09:56 -0500

    Added function to count points within a range

--- R/rapt-spec.R
+++ R/rapt-spec.R
@@ -2,11 +2,30 @@
 # This file contains methods for working with the mass spectrum of APT data
 #
 
-# Define ranges in MassSpectrum
-rangeSpec <- function(form, charge = 1:4, resolution = 400, thresh = 20) {
-  if(is(form, "cform")) {
-    cform <- as.data.frame(t(form));
-    form <- form["formula", ];
+#### cformPat ####
+#' Generate the isotopic positions and intensities from a \code{cform}
+#' object.
+cformPat <- function(cform, charge = 1:4, thresh = 20) {
+  if(is(cform, "cform")) {
+    form <- t(as.character(cform$formula));
+  } else {
+    warning("The given formulae are not of class cform")
+  }
+  rng.pat <- lapply(charge, function(x) {
+    enviPat::isopattern(
+      rapt::isotopes, chemforms = form,
+      threshold = thresh, charge = x,
+      verbose = F
+    )
+  })
+  return(rng.pat)
+}
+
+#### rangeSpec ####
+#' Define ranges in a \code{\link[MALDIquant]{MassSpectrum}}.
+rangeSpec <- function(cform, charge = 1:4, resolution = 400, thresh = 20) {
+  if(is(cform, "cform")) {
+    form <- as.character(cform$formula);
   } else {
     warning("The given formulae are not of class cform")
   }
@@ -42,29 +61,41 @@ rangeSpec <- function(form, charge = 1:4, resolution = 400, thresh = 20) {
   rownames(rng.dat) <- apply(rng.dat, 1, function(x){
     paste(c(x["formula"], x["charge"]), collapse = "+")
   })
-  if (exists("cform")) {
-    rng.dat$color <- as.character(
-      cform$color[match(rng.dat$formula, cform$formula)])
-  } else {
-    rng.dat$color <- rep_len(NA, dim(rng.dat)[1])
-  }
+  # if (exists("cform")) {
+  #   rng.dat$color <- as.character(
+  #     cform$color[match(rng.dat$formula, cform$formula)])
+  # } else {
+  #   rng.dat$color <- rep_len(NA, dim(rng.dat)[1])
+  # }
   return(rng.dat);
 }
-# Merge RNG objects
+#### mergeRange ####
+#' Merge RNG objects
 mergeRange <- function(rng1, rng2) {
 
 }
-# Remove ranges from an RNG
+#### removeRange ####
+#' Remove ranges from an RNG
 removeRange <- function(rng, rem) {
   rng.sub <- setdiff(rownames(rng), rem);
   rng.new <- rng[rng.sub, ];
   return(rng.new);
 }
-# Create a reference MassPeaks object for warping
+
+#### rangeCount ####
+#' Count the hits within a mass range
+rangeCount <- function(pos, start, end) {
+  n <- with(pos, nrow(pos[mass > start & mass < end,]))
+  return(n)
+}
+
+#### refSpec ####
+#' Create a reference \code{\link[MALDIquant]{MassPeaks}} object for warping.
 refSpec <- function(form, charge = 1:4, resolution = 400) {
   if(is(form, "cform")) {
-    cform <- as.data.frame(t(form));
-    form <- form["formula", ];
+    form <- form[,'formula'];
+  } else {
+    warning('Object form is not of class cform.')
   }
   ref.pat <- lapply(charge, function(x){
     enviPat::isopattern(
@@ -87,23 +118,26 @@ refSpec <- function(form, charge = 1:4, resolution = 400) {
   ref.dat <- createMassPeaks(ref.vdet[, "m/z"], ref.vdet[, "abundance"])
   return(ref.dat)
 }
-# Auto define ranges from a MassPeaks object
-rangePeaks <- function(spec, peaks, cut = 0.1) {
+#### rangePeaks ####
+#' Automatically define ranges from a \code{\link[MALDIquant]{MassPeaks}}
+#' object.
+rangePeaks <- function(spec, peaks, cut = 0.1, sig = 1) {
   if(!isMassSpectrum(spec)) {
     stop('spec is not a MassSpectrum object')
   }
   if(!isMassPeaks(peaks)) {
     stop('peaks is not a MassPeaks object')
   }
-  rng.ind <- match(peaks@mass, spec@mass)
+  rng.ind <- match(signif(peaks@mass, digits = sig),
+                   signif(spec@mass, digits = sig))
   rng.len <- length(spec@intensity)
   rng.bounds <- sapply(rng.ind, function(x) {
-    max.ind <- which(spec@intensity[-seq(0, x)] - cut*spec@intensity[x] < 0,
-                     arr.ind = T)[1]
+    max.ind <- rng.len[
+      which(spec@intensity[-seq(0, x)] - cut*spec@intensity[x] < 0)][1]
     max.val <- spec@mass[-seq(0, x)][max.ind]
-    min.ind <- rev(
-      which(spec@intensity[-seq(x, rng.len)] - cut*spec@intensity[x] < 0,
-            arr.ind = T))[1]
+    min.ind <- rng.len[rev(
+      which(spec@intensity[-seq(x, rng.len)] - cut*spec@intensity[x] < 0)
+      )][1]
     min.val <- spec@mass[-seq(x, rng.len)][min.ind]
     c(min.val,max.val)
   })
@@ -114,4 +148,28 @@ rangePeaks <- function(spec, peaks, cut = 0.1) {
                         color = rep_len(NA, length(peaks))
                         )
   return(rng.dat)
+}
+#### fragments ####
+#' Calculate all (combinatorically) possible fragments of a chemical formula.
+fragments <- function(chemform) {
+  repAtom <- function(char, len) {
+    Hmisc::makeNstr(char, 0:len)
+  }
+  mol <- enviPat::check_chemform(isotopes, chemform, get_sorted = T)
+  if(any(mol$warning)) {
+    stop('An invalid chemform was supplied.')
+  }
+  atom <- enviPat::check_chemform(isotopes, mol$new_formula, get_list = T)
+  reps <- lapply(atom, function(m) {
+    mapply(repAtom, char = names(m), len = m)
+  })
+  grid <- lapply(reps,expand.grid)
+  frag <- lapply(grid, function(g) {
+    s <- apply(g, 1, paste0, collapse = '')
+    return(s[-1])
+  })
+  dat <- lapply(frag, function(f) {
+    enviPat::check_chemform(isotopes, f, get_sorted = T)$new_formula
+  })
+  return(dat)
 }
