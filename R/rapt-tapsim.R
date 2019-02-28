ffc7f9c2904ea2eb21ffc77f4e9d0cb44b255b57
Andrew Proudian <aproudia@mines.edu>
2019-02-28 14:36:48 -0700

    Minimum working code. Only implemented for results files.

--- R/rapt-tapsim.R
+++ R/rapt-tapsim.R
@@ -1,3 +1,21 @@
-library(Rcpp)
+library(Rcpp11)
+sourceCpp('tapsim_reader.cpp')
 
-readTAPSim <- function(fp, type = "results")
+#### readTAPSim ####
+#' Read a TAPSim results file
+#'
+#' Given a filepath, read in a TAPSim file. Currently only implemented for
+#' results.
+#'
+#' @param fp The filepath
+#' @param type The file type to be read. Currently, only "results" is
+#' implemented.
+#' @return A data.frame
+#' @seealso \url{
+#'   http://www.uni-stuttgart.de/imw/mp/forschung/atom_probe_RD_center/howto.pdf
+#' }
+#' @export
+readTAPSim <- function(fp, type = 'results') {
+  df <- read_TAPSim(fp, type)
+  return(df)
+}
