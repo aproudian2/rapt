c31fdccc37c5aa99cdbd142a69075c056c0291ae
Andrew Proudian <aproudia@mines.edu>
2019-06-25 16:19:58 -0600

    Documentation

--- R/rapt-spec.R
+++ R/rapt-spec.R
@@ -26,8 +26,8 @@ rangeCount <- function(pos, start, end) {
 #' @param pos A data.frame. The pos to be ranged
 #' @param start The start of the mass range
 #' @param end The end of the mass range
-#' @return A data.frame of the same structure as pos containing only hits in the
-#' provided range
+#' @return A data.frame of the same structure as \code{pos} containing only hits
+#' in the provided range
 #' @export
 rangePOS <- function(pos, start, end) {
   with(pos, pos[mass > start & mass < end,])
