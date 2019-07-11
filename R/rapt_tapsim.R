1060f01a6bc1e735f9d3ee452b1cff6f6b6205ab
Andrew Proudian <aproudia@mines.edu>
2019-07-11 13:58:36 -0600

    Added functions to convert a result to a POS (resultToPOS) and a detector hit map (resultToDet).

--- R/rapt_tapsim.R
+++ R/rapt_tapsim.R
@@ -34,3 +34,39 @@ readResult <- function(fp, type = 'ASCII') {
     } else {}
     return(dat)
 }
+
+#### resultToPOS ####
+#' Convert a TAPSim results file to a POS
+#'
+#' @param res The results data.frame
+#' @param clip.radius The detector radius at which to clip the points
+#' @return A data.frame containing the (x,y,z) position and "mass' (id) of the
+#'   point in the stame structure of a POS as created by \code{\link{readPOS}}.
+#' @seealso \code{\link{readPOS}}, \code{\link{readResult}}
+#' @export
+resultToPOS <- function(res, clip.radius = NULL) {
+    pos <- with(res, data.frame(x = startX, y = startY, z = startZ, mass = id))
+    if (!is.null(clip.radius)) {
+        r <- with(res, sqrt(stopX^2 + stopY^2))
+        pos <- pos[r <= clip.radius,]
+    }
+    return(pos)
+}
+
+#### resultToDet ####
+#' Convert a TAPSim results file to a detector point pattern
+#'
+#' @param res The results data.frame
+#' @param clip.radius The detector radius at which to clip the points
+#' @return A ppp containing the detector positions marked by their id
+#' @seealso \code{\link{createDet}}, \code{\link{readResult}}
+#' @export
+resultToDet <- function(res, clip.radius = NULL) {
+    det.df <- with(res, data.frame(dx = stopX, dy = stopY, mass = id))
+    det <- createDet(det.df)
+    marks(det) = det.df$mass
+    if (!is.null(clip.radius)) {
+        Window(det) <- disc(radius = clip.radius)
+    }
+    return(det)
+}
