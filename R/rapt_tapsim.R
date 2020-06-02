deeb78696902b391302e7fc73a74b8289cea9e10
Andrew Proudian <aproudia@mines.edu>
2020-06-02 12:04:43 -0600

    Updated documentation. Removed with statements

--- R/rapt_tapsim.R
+++ R/rapt_tapsim.R
@@ -1,16 +1,26 @@
-#### readResults ####
-#' Read a TAPSim results file
+#
+# This file contains functions pertaining to TAPSim
+#
+
+#### Read Data ####
+
+### readResult ###
+#' Read TAPSim Results
 #'
-#' Given a filepath, read in a TAPSim results file. Currently only implemented
-#' for ASCII results files.
+#' Given a filepath, read in a TAPSim results file.
+#' \emph{Currently only implemented for ASCII results files.}
 #'
-#' @param fp The filepath
-#' @param type The file type to be read. Currently, only ASCII is implemented.
-#' @return A data.frame
+#' @param fp Character. The filepath
+#' @param type Character. The file type to be read. Currently, only ASCII is
+#'   implemented.
+#' @return A data.frame with the structure of the TAPSim output.
+#'
+#' @family TAPSim functions
 #' @seealso \url{
 #'   http://www.uni-stuttgart.de/imw/mp/forschung/atom_probe_RD_center/howto.pdf
 #' }
 #' @seealso \url{https://d-nb.info/1138282715/34}
+#'
 #' @export
 readResult <- function(fp, type = 'ASCII') {
     txt <- readLines(fp, n = 100)  # Not ideal to hard code the max...
@@ -18,54 +28,68 @@ readResult <- function(fp, type = 'ASCII') {
     dat <- read.delim(fp, header = FALSE, skip = skip)
     n <- ncol(dat)
     if(n == 26) {
-        names(dat) <- c('index','id','number','voltage',
-                        'startX','startY','startZ',
-                        'stopX','stopY','stopZ','tof','probability',
-                        'potentialBefore',
-                        'fieldBeforeX','fieldBeforeY','fieldBeforeZ',
-                        'potentialAfter',
-                        'fieldAfterX','fieldAfterY','fieldAfterZ',
-                        'normalX','normalY','normalZ','apexX','apexY','apexZ')
+        names(dat) <- c("index","id","number","voltage",
+                        "startX","startY","startZ",
+                        "stopX","stopY","stopZ","tof","probability",
+                        "potentialBefore",
+                        "fieldBeforeX","fieldBeforeY","fieldBeforeZ",
+                        "potentialAfter",
+                        "fieldAfterX","fieldAfterY","fieldAfterZ",
+                        "normalX","normalY","normalZ","apexX","apexY","apexZ")
     } else if(n == 18) {
-        names(dat) <- c('index','id','number','voltage',
-                        'startX','startY','startZ',
-                        'stopX','stopY','stopZ','tof','probability',
-                        'normalX','normalY','normalZ','apexX','apexY','apexZ')
-    } else {}
+        names(dat) <- c("index","id","number","voltage",
+                        "startX","startY","startZ",
+                        "stopX","stopY","stopZ","tof","probability",
+                        "normalX","normalY","normalZ","apexX","apexY","apexZ")
+    } else {
+        warning("Unknown file structure. Returning unnamed columns.")
+    }
     return(dat)
 }
 
-#### resultToPOS ####
-#' Convert a TAPSim results file to a POS
+#### Condition Data ####
+
+### resultToPOS ###
+#' Convert TAPSim Results to POS
 #'
-#' @param res The results data.frame
-#' @param clip.radius The detector radius at which to clip the points
-#' @return A data.frame containing the (x,y,z) position and "mass' (id) of the
-#'   point in the stame structure of a POS as created by \code{\link{readPOS}}.
-#' @seealso \code{\link{readPOS}}, \code{\link{readResult}}
+#' @param res data.frame. The results \code{data.frame} as returned by
+#'   \code{\link{readResult}}
+#' @param clip.radius Numeric. The detector radius at which to clip the points
+#' @return A \code{data.frame} containing the (x,y,z) position and "mass" (id)
+#'   of the point in the stame structure of a POS as created by
+#'   \code{\link{readPOS}}.
+#'
+#' @family TAPSim functions
+#' @seealso \code{\link{readPOS}}
+#'
 #' @export
 resultToPOS <- function(res, clip.radius = NULL) {
-    pos <- with(res, data.frame(x = startX*1e9,
-                                y = startY*1e9,
-                                z = startZ*1e9,
-                                mass = id))
+    pos <- data.frame(x = res$startX*1e9,
+                      y = res$startY*1e9,
+                      z = res$startZ*1e9,
+                      mass = res$id)
     if (!is.null(clip.radius)) {
-        r <- with(res, sqrt(stopX^2 + stopY^2))
+        r <- sqrt(res$stopX^2 + res$stopY^2)
         pos <- pos[r <= clip.radius,]
     }
     return(pos)
 }
 
-#### resultToDet ####
-#' Convert a TAPSim results file to a detector point pattern
+### resultToDet ###
+#' Convert TAPSim Result to Detector \code{\link[spatstat]{ppp}}
 #'
-#' @param res The results data.frame
-#' @param clip.radius The detector radius at which to clip the points
-#' @return A ppp containing the detector positions marked by their id
-#' @seealso \code{\link{createDet}}, \code{\link{readResult}}
+#' @param res data.frame. The results \code{data.frame} as returned by
+#'   \code{\link{readResult}}
+#' @param clip.radius Numeric. The detector radius at which to clip the points.
+#' @return A \code{\link[spatstat]{ppp}} containing the detector positions
+#'   marked by their id.
+#'
+#' @family TAPSim functions
+#' @seealso \code{\link{createDet}}
+#'
 #' @export
 resultToDet <- function(res, clip.radius = NULL) {
-    det.df <- with(res, data.frame(dx = stopX, dy = stopY, mass = id))
+    det.df <- data.frame(dx = res$stopX, dy = res$stopY, mass = res$id)
     det <- createDet(det.df)
     marks(det) = det.df$mass
     if (!is.null(clip.radius)) {
